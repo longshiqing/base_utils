1794d216603bb021420c90867646d77fcc7a75b0
nicajonh <nicajonh@126.com>
2016-06-01 15:32:16 +0800

    fix igonre

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
 		name='base_utils',
 		version='1.0.14',
 		description='utils',
-		author_email='cnhzhicong@gmail.com',
+		author_email='nicajonh@gmail.com',
 		url='http://www.xxx.cc',
 		packages=['base_utils'],
 		long_description='utils, include time utils, string utils, encrypt utils, cache, rsa, logger and so on',
